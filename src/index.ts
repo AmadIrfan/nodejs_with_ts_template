11d33e95cd0b622caf99e2c75b14dab131e5b680
amadirfan <amadirfan443@gmail.com>
2024-12-28 23:34:07 +0500

    update template

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-import express from "express";
+/** @format */
 
+import express from "express";
+import { config } from "dotenv";
+import path from "path";
+config();
 const app = express();
 
 app.use(express.json());
 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.get("/", (req: any, res: any) => {
-	res.send("Welcome to my app!!!!!!!");
+	res.render('index');
+	// res.send("Welcome to my app!!!!!!!");
 });
 
-app.listen(8000, () => {
-	console.log("Server started on port 8000");
+app.listen(process.env.PORT, () => {
+	console.log(`Server started on port : ${process.env.PORT}`);
+	console.log(`http://localhost:${process.env.PORT}/`);
 });
